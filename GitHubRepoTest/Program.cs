43e74b955e3c65db2d14b58ffe0178545c95ecda
Alonso Canedo <aac0023@mix.wvu.edu>
2021-02-03 12:07:05 -0500

    Add project files.

---
+++ GitHubRepoTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubRepoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
